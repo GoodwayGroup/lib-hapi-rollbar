d52cff7bbd9e5929dbe310379b2f8da1480f8fc2
Derek Smith <dsmith@goodwaygroup.com>
2019-06-27 12:11:14 -0500

    Version updates and security patching (#9)
    * added jest, pre codemods run
    
    * Update dependencies and rollbar version

--- spec/support/junit.js
+++
@@ -1,8 +0,0 @@
-const reporters = require('jasmine-reporters');
-const path = require('path');
-
-const junitReporter = new reporters.JUnitXmlReporter({
-    savePath: path.join(__dirname, '..', '..', 'coverage', 'junit'),
-    consolidateAll: false
-});
-jasmine.getEnv().addReporter(junitReporter);
